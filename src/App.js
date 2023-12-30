b22f8bd8e882f55529a37224c3a906eefefcc982
Cholan Madala <cholanmadala1510@gmail.com>
2023-12-30 19:55:51 +0530

    [CS] add event handlers, css changes to Button Component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,48 @@ import './index.css';
 import Button from "./Button";
 
 function App() {
+
+  const primaryButtonClickHandler = ev => {
+    console.log(ev, 'primary button handler');
+  }
+  const secondaryButtonClickHandler = ev => {
+    console.log(ev, 'secondary button handler');
+  }
+
   return (
-    <div>
-      <div><Button primary outlined>Primary</Button></div>
-      <div><Button secondary rounded>Secondary</Button></div>
-      <div><Button success rounded outlined>Success!!</Button></div>
+    <div className='grid gap-1 grid-cols-4 grid-rows-8'>
+      <section>
+      <div><Button >Plain</Button></div>
+      <div><Button className='mb-10 mt-10 ml-10 mr-10' primary onClick={primaryButtonClickHandler}>Primary</Button></div>
+      <div><Button secondary onClick={secondaryButtonClickHandler}>Secondary</Button></div>
+      <div><Button success>Success!!</Button></div>
       <div><Button warning>Warning</Button></div>
+      <div><Button danger>Danger!!</Button></div>
+      </section>
+      <section>
+      <div><Button rounded>Plain</Button></div>
+      <div><Button primary rounded onClick={primaryButtonClickHandler}>Primary</Button></div>
+      <div><Button secondary rounded onClick={secondaryButtonClickHandler}>Secondary</Button></div>
+      <div><Button success rounded>Success!!</Button></div>
+      <div><Button warning rounded>Warning</Button></div>
+      <div><Button danger rounded>Danger!!</Button></div>
+      </section>
+      <section>
+      <div><Button rounded outlined>Plain</Button></div>
+      <div><Button primary rounded outlined>Primary</Button></div>
+      <div><Button secondary rounded outlined>Secondary</Button></div>
+      <div><Button success rounded outlined>Success!!</Button></div>
+      <div><Button warning rounded outlined>Warning</Button></div>
       <div><Button danger rounded outlined>Danger!!</Button></div>
+      </section>
+      <section>
+      <div><Button outlined>Plain</Button></div>
+      <div><Button primary outlined>Primary</Button></div>
+      <div><Button secondary outlined>Secondary</Button></div>
+      <div><Button success outlined>Success!!</Button></div>
+      <div><Button warning outlined>Warning</Button></div>
+      <div><Button danger outlined>Danger!!</Button></div>
+      </section>
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,43 +1,53 @@
 import Proptypes from 'prop-types';
 import className from 'classnames';
 
-function Button({rounded, children, outlined, primary, secondary, success, warning, danger}) {    
-        
-    const classes = className(
-        'w-full', 'md:w-auto',
-        'px-14', 'py-8',
-        'mx-2', 'my-2', 
-        'text-zinc-50', 'text-2xl',
-        'border','border-black',
-        {
-            'bg-blue-700': primary,
-            'bg-blue-300': secondary,
-            'bg-green-700': success,
-            'bg-yellow-400': warning,
-            'bg-red-600': danger,
-            'outline': outlined,
-            'rounded-lg': rounded
-        },
-        );
+function Button({ rounded, children, outlined, primary, secondary, success, warning, danger, ...rest }) {
+	
+	const classes = className(
+		rest.className,
+		'w-full', 'md:w-auto',
+		'px-10', 'py-4',
+		'mx-2', 'my-2', 'text-2xl',
+		'border-4',
+		{
+			'bg-blue-700 border-blue-700': primary,
+			'bg-gray-600 border-gray-600': secondary,
+			'bg-green-500 border-green-500 ': success,
+			'bg-yellow-400 border-yellow-400 ': warning,
+			'bg-red-500 border-red-500 ': danger,
+			'rounded-full': rounded,
+			'bg-white outline': outlined,
+			'text-blue-500': outlined && primary,
+			'text-gray-900': outlined && secondary,
+			'text-green-500': outlined && success,
+			'text-yellow-400': outlined && warning,
+			'text-red-500': outlined && danger
+		}
+	);
 
-    return (
-        <button className={classes}>{children}</button>
-    )
+	return (
+		<button 
+			{...rest}
+			className={classes}	
+		>
+			{children}
+		</button>
+	)
 }
 
 Button.propTypes = {
-    checkVariationValue : ({primary, secondary, success, warning, danger}) => {
-        const count = Number(!!primary) +
-        Number(!!secondary) +
-        Number(!!success) + 
-        Number(!!warning) +
-        Number(!!danger) 
+	checkVariationValue: ({ primary, secondary, success, warning, danger }) => {
+		const count = Number(!!primary) +
+			Number(!!secondary) +
+			Number(!!success) +
+			Number(!!warning) +
+			Number(!!danger)
 
-        if (count > 1) {
-            return new Error('Only one among primary, secondary, success, warning and danger need to be true.');
-        }
-    },
-    rounded: Proptypes.bool,
-    outlined: Proptypes.bool
+		if (count > 1) {
+			return new Error('Only one among primary, secondary, success, warning and danger need to be true.');
+		}
+	},
+	rounded: Proptypes.bool,
+	outlined: Proptypes.bool
 };
 export default Button;
